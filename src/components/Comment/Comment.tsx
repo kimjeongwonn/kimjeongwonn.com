eec60b2c99331a175c7b81041b0d072e9e83cba6
kimjeongwonn <kimjeongwonn@gmail.com>
2023-02-12 17:18:15 +0900

    fix: ref 콜백이 두 번 중복 호출되서 댓글창이 두개가 나오는 문제 수정

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,24 +1,23 @@
+import { useCallback } from 'react';
 import { CommentSection } from './Comment.styles';
 
 const Comment = () => {
-  return (
-    <CommentSection
-      ref={elem => {
-        if (!elem) {
-          return;
-        }
-        const scriptElem = document.createElement('script');
-        scriptElem.src = 'https://utteranc.es/client.js';
-        scriptElem.async = true;
-        scriptElem.setAttribute('repo', 'kimjeongwonn/kimjeongwonn.com');
-        scriptElem.setAttribute('issue-term', 'pathname');
-        scriptElem.setAttribute('theme', 'github-light');
-        scriptElem.setAttribute('label', 'blog-comment');
-        scriptElem.crossOrigin = 'anonymous';
-        elem.appendChild(scriptElem);
-      }}
-    />
-  );
+  const refCallback = useCallback((elem: HTMLDivElement | null) => {
+    if (!elem) {
+      return;
+    }
+    const scriptElem = document.createElement('script');
+    scriptElem.src = 'https://utteranc.es/client.js';
+    scriptElem.async = true;
+    scriptElem.setAttribute('repo', 'kimjeongwonn/kimjeongwonn.com');
+    scriptElem.setAttribute('issue-term', 'pathname');
+    scriptElem.setAttribute('theme', 'github-light');
+    scriptElem.setAttribute('label', 'blog-comment');
+    scriptElem.crossOrigin = 'anonymous';
+    elem.appendChild(scriptElem);
+  }, []);
+
+  return <CommentSection ref={refCallback} />;
 };
 
 export default Comment;
